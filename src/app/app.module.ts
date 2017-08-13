5dbcd0f725ef0dc0b786bba9a8c6986e2eedd82f
Anurag Bhattacharjee <anurag07061@gmail.com>
2017-08-14 03:02:30 +0600

    First commit: skeleton of the app, basic tts and speeech recognition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech'
+import { SpeechRecognition } from '@ionic-native/speech-recognition'
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -24,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TextToSpeech,
+    SpeechRecognition
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
+
 import { Component } from '@angular/core';
+import {TextToSpeech} from '@ionic-native/text-to-speech';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition, SpeechRecognitionListeningOptionsAndroid, SpeechRecognitionListeningOptionsIOS } from '@ionic-native/speech-recognition'
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,76 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  text: string;
+  speechList : Array<string> = [];
+  androidOptions: SpeechRecognitionListeningOptionsAndroid;
+  iosOptions: SpeechRecognitionListeningOptionsIOS;
 
+  constructor(private platform: Platform, private tts: TextToSpeech, private speech: SpeechRecognition, public navCtrl: NavController) {
+
+  }
+
+  listenForSpeech(): void {
+
+    this.androidOptions = {
+      prompt: 'Speak into your phone!'
+    }
+
+    this.iosOptions = {
+      language: 'en-US'
+    }
+
+    if (this.platform.is('android')) {
+      this.speech.startListening(this.androidOptions).subscribe(data => this.speechList = data, error => console.log(error));
+    }
+    else if (this.platform.is('ios')) {
+      this.speech.startListening(this.iosOptions).subscribe(data => this.speechList = data, error => console.log(error));
+    }
+  }
+
+  async isSpeechSupported(): Promise<boolean> {
+    let isAvailable = await this.speech.isRecognitionAvailable();
+    console.log(isAvailable);
+    return isAvailable;
+  }
+  async getPermission(): Promise<void> {
+    try {
+      let permission = await this.speech.requestPermission();
+      console.log(permission);
+      return permission;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+  async hasPermission(): Promise<boolean> {
+    try {
+      let permission = await this.speech.hasPermission();
+      console.log(permission);
+      return permission;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+  async getSupportedLanguages(): Promise<Array<string>> {
+    try {
+      let languages = await this.speech.getSupportedLanguages();
+      console.log(languages);
+      return languages;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  async sayText():Promise<any>{
+    try{
+      await this.tts.speak(this.text);
+    }
+    catch(e){
+      console.log(e);
+    }
   }
 
 }
